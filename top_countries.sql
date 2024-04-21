1418f2c3efc1280e9e78a2e3b76bbfbe221a2c4f
Nick Cruz <nick@dhcp020-192-138.wireless.buffalo.edu>
2024-04-21 11:58:01 -0400

    add sql queries

---
+++ top_countries.sql
@@ -0,0 +1,2 @@
+select playlists.track_id,playlists.track_name,count(playlists.name) as num_countries from playlists join tracks on playlists.track_id=tracks.track_id
+group by (playlists.track_id,playlists.track_name) order by num_countries desc
