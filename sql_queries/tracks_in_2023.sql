7b1dc0a071eb8356c8e97fcbe4b315212d816a03
Caballesjason <jasoncaballes99@gmail.com>
2024-04-27 16:01:22 -0400

    - Deleted the jason's ipynb file

--- sql_queries/tracks_in_2023.sql
+++ sql_queries/tracks_in_2023.sql
@@ -1,1 +1,5 @@
-select * from tracks join albums on tracks.album_id=albums.album_id where strftime('%Y', albums.date_released) = '2023'
+select *
+from tracks
+join albums
+	on tracks.album_id=albums.album_id
+where SUBSTRING(ALBUMS.date_released, 1, POSITION('-' IN 'YYYY-MM-DD') - 1)= '2023'
