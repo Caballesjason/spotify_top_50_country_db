93cd66dd2d1e18daf3c35960f1d2230dbb8b738b
Caballesjason <jasoncaballes99@gmail.com>
2024-04-27 17:20:25 -0400

    FIXED tracks_in_2023.sql

--- sql_queries/tracks_in_2023.sql
+++ sql_queries/tracks_in_2023.sql
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-select *
-from tracks
-join albums
-	on tracks.album_id=albums.album_id
-where SUBSTRING(ALBUMS.date_released, 1, POSITION('-' IN 'YYYY-MM-DD') - 1)= '2023'
-=======
-select artist_name,max(popularity) max_popularity,year,album_id from (select *,strftime('%Y', date_released) year from AlbumArtists join artists on AlbumArtists.artist_id=Artists.artist_id 
-join Albums on AlbumArtists.album_id=Albums.album_id) album_join_artist group by year
->>>>>>> ce7c479 (add query)
-=======
 -- Find all tracks created in 2023
 SELECT *
 FROM tracks
 JOIN albums
 	ON tracks.album_id=albums.album_id
-WHERE SUBSTRING(ALBUMS.date_released, 1, POSITION('-' IN 'YYYY-MM-DD') - 1)= '2023'
->>>>>>> 085c784 (- Added queries)
+WHERE LEFT(ALBUMS.date_released, 4)= '2023'
+
